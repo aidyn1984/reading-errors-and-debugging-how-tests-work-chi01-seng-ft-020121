76c207bd971fda7c4d2e277236d6859b06216145
Val_1 S_z <csa.aiden@gmail.com>
2021-01-22 09:11:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,16 @@
-# Add your variables here
-first_number = 1
-second_number = 3
-sum =(first_number + second_number)
+first_number = 4
+second_number = 2
 
+sum =(first_number * second_number)
+
+first_number = 4
+second_number = 2
+difference =(first_number - second_number)
+
+first_number = 4
+second_number = 2
+product =(first_number * second_number)
+
+first_number = 4
+second_number = 2
+quotient=(first_number / second_number)
